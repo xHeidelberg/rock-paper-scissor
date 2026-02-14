2d17473bce01e1b9484c34da2e0233aea0d67399
Ded-Laptop <167631120+xHeidelberg@users.noreply.github.com>
2026-02-14 22:12:25 +0800

    add insert new image, edit html base and js script

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
 const validChoice = ["rock", "paper", "scissors"];
 let resultIn = document.getElementById("gameResult");
 let clickAction = document.querySelector("#buttonHuman");
-clickAction.addEventListener('click', playGame);
+
+// Select TODO
+let pickingResult = document.querySelectorAll('#selectedPick');
+pickingResult.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let baseHuman = document.querySelector('.pickBase');
+        let holder = e.target.id.toLowerCase().trim();
+
+        switch (holder) {
+            case 'rock':
+                whatSelected(e.target.id)
+                baseHuman.style.backgroundImage = 'url(./images/rock.png)';
+                break;
+            case 'paper':
+                whatSelected(e.target.id)
+                baseHuman.style.backgroundImage = 'url(./images/paper.png)';
+                break;
+            case 'scissor':
+                whatSelected(e.target.id)
+                baseHuman.style.backgroundImage = 'url(./images/scissor.png)';
+                break;
+        }
+
+    });
+}
+);
+
+// not moving if hindi gumaganaaa!!
 
 
-function playGame(){
-    let playRound = 5;
-    let humanScore = 0;
+function whatSelected(id) {
+    let para = document.querySelector('#resultHere p')
+    let divResult = document.querySelector('#resultHere');
+    let spanner = document.createElement('span');
+
+    spanner.textContent = `Result: ${id}`;
+    if (para) {
+        para.textContent = '';
+        para.appendChild(spanner);
+    }
+    else {
+        let newPara = document.createElement('p')
+        newPara.appendChild(spanner);
+        divResult.appendChild(spanner);
+    }
+};
+
+
+//clickAction.addEventListener('click', playGame);
+function playGame() {
+    // let humanScore = 0;
     let computerScore = 0;
     let drawScore = 0;
 
     const human = getHumanChoice;
     const computer = getComputerChoice;
 
-    function getComputerChoice(){
+    function getComputerChoice() {
         let randoming = Math.floor(Math.random() * validChoice.length) + 1;
 
         if (randoming === 1)
@@ -23,69 +68,69 @@ function playGame(){
         else
             return "scissors";
     }
-
-    function getHumanChoice(){
+/*
+    function getHumanChoice() {
         const pick = prompt("Pick (Rock, Paper or Scissors): ");
-        try{
-            if (validChoice.includes(pick.toLowerCase())){
+        try {
+            if (validChoice.includes(pick.toLowerCase())) {
                 return pick.toLowerCase();
             }
-        } 
-        catch(error){
+        }
+        catch (error) {
             return;
         }
-        
-    }
 
-    function playGround(human, computer){
-        if (human == "rock"){
-            if (computer === "paper"){
+    }
+*/
+    function playGround(human, computer) {
+        if (human == "rock") {
+            if (computer === "paper") {
                 console.log("Computer Wins!");
                 computerScore++;
             }
-            else if (computer === "scissors"){
+            else if (computer === "scissors") {
                 console.log("Player Wins!");
                 humanScore++;
             }
-            else{
+            else {
                 console.log("Draw!");
                 drawScore++;
             }
         }
-        else if (human === "paper"){
-            if (computer === "rock"){
+        else if (human === "paper") {
+            if (computer === "rock") {
                 console.log("Player Wins!");
                 humanScore++;
             }
-            else if (computer === "scissors"){
+            else if (computer === "scissors") {
                 console.log("Computer Wins!");
                 computerScore++;
             }
-            else{
+            else {
                 console.log("Draw!");
                 drawScore++
             }
         }
-        else{
-            if (computer === "rock"){
+        else {
+            if (computer === "rock") {
                 console.log("Computer Wins!");
                 computerScore++;
             }
-            else if (computer === "paper"){
+            else if (computer === "paper") {
                 console.log("Player Wins!");
                 humanScore++;
             }
-            else{
+            else {
                 console.log("Draw!");
                 drawScore++
             }
         }
     }
 
-for(let i = 1; i <= playRound; i++){
+    for (let i = 1; i <= playRound; i++) {
         const a = getHumanChoice();
         const b = getComputerChoice();
         playGround(a, b);
     }
-    resultIn.innerHTML = `Scores: Player: ${humanScore} | Computer: ${computerScore} | Draw: ${drawScore}`;gi
+    resultIn.innerHTML = `Scores: Player: ${humanScore} | Computer: ${computerScore} | Draw: ${drawScore}`; gi
 }
